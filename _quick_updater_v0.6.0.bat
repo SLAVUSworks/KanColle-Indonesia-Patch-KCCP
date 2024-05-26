c3551738bc796c91541874e70a96f609cc5051f5
ShiiBuukii <shigure.to.fubuki@gmail.com>
2024-05-26 23:49:05 +0700

    Refactor

--- _quick_updater_v0.6.0.bat
+++ _quick_updater_v0.6.0.bat
@@ -1,6 +1,6 @@
 @echo off
  
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ". 'EN-patch-manager\quick_updater.ps1'"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ". 'EN-patch-manager\clear_browser_cache.ps1'"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ". 'ID-patch-manager\quick_updater.ps1'"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ". 'ID-patch-manager\clear_browser_cache.ps1'"
  
 TIMEOUT /T 60
