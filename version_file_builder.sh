aea11ccfd155546f9782fa4bb2efe6d77f9d1fe1
terukaze1939 <shigure.to.fubuuki@gmail.com>
2024-08-02 02:38:03 +0700

    Update to v3.22.18.1
        -== Raw Text Patching ==-
        -- Update --
        - Ship data
        - Equipment data
        - Quest data
        - Item data
    
        -- Added --
        - Summer 2024 Event MO data
        - Item menu "Extra Items" asset
    
        -- Fixed --
        - 14inch/45 Twin/Triple Gun Mount regex
        - C60/Cy6 quest now shows the correct requirements
        - Cy12 and Cy13 names and descriptions have been swapped
    
        -== Texture Patching ==-
        -- Update --
        - General equipment assets
    
        -- Added --
        - Summer 2024 Event MO assets

---
+++ version_file_builder.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+diff="diff.txt"
+
+
+
+if [ ! -f "$diff" ]; then
+    echo "File diff.txt tidak ada!" >&2
+    exit 1
+fi
+
+if [ ! -s "$diff" ]; then
+    echo "File diff.txt kosong!" >&2
+    exit 1
+fi
+
+arrayFromFile=()
+listDel=()
+listAdd=()
+
+echo "Masukan versi yang baru: "
+read version
+
+
+# Membuat sebuah list ke dalam arrayFromFile
+while IFS= read -r path || [ -n "$path" ]; do
+    arrayFromFile+=("$path")
+done < "$diff"
+
+# Memproses setiap baris dari arrayFromFile
+for line in "${arrayFromFile[@]}"; do
+    type=$(echo "$line" | awk '{print $1}')
+    file=$(echo "$line" | awk '{print $2}')
+
+    if [ "$type" == "D" ]; then
+        listDel+=("\"$file\"")
+    elif [ "$type" == "A" -o "$type" == "M" ]; then
+        listAdd+=("\"$file\"")
+    fi
+done
+
+# Buat separator setiap nama path dengan separator "," 
+strDel=$(IFS=,;echo "${listDel[*]}")
+strAdd=$(IFS=,;echo "${listAdd[*]}")
+
+# buat object 
+objects="{\"version\": \"$version\", \"del\": ["$strDel"], \"add\": ["$strAdd"]}"
+
+# simpan object menjadi file
+echo "$objects" > new_object.json
+
+jq '. += [inputs]' version.json new_object.json > temp.json && mv temp.json version.json
+
+rm new_object.json
